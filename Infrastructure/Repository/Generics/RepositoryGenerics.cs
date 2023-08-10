cd53f36e9e169abdd0ffec8bd20d39815daf327c
DevThiagoSouza <thiago.soares.de.souza@hotmail.com>
2023-08-10 17:43:54 -0300

    add Igenric

--- Infrastructure/Repository/Generics/RepositoryGenerics.cs
+++ Infrastructure/Repository/Generics/RepositoryGenerics.cs
@@ -5,9 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using Domain.Interfaces.Generics;
 
+
 namespace Infrastructure.Repository.Generics
 {
-    public class RepositoryGenerics<T> : IGeneric
+    public class RepositoryGenerics<T> : IGeneric<T>, IDisposable where T : class
     {
+        public Task Add(T Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(T Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(T Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetEntityById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
